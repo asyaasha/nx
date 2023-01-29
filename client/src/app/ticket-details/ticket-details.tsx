1c69b958dd4787577b9d088f7fbab19d6fe9ae41
Asiya <asya.asha@gmail.com>
2023-01-29 14:47:55 -0500

    fetch ticket details by id

--- client/src/app/ticket-details/ticket-details.tsx
+++ client/src/app/ticket-details/ticket-details.tsx
@@ -1,22 +1,30 @@
+import { useParams } from "react-router-dom";
 import { Ticket } from '@acme/shared-models';
 
 import styles from './ticket-details.module.css';
-
+import {useGetTicketByIdQuery  } from '../services/ticketsApi';
 /* eslint-disable-next-line */
 export interface TicketDetailsProps {
 }
 
 export function TicketDetails(props: TicketDetailsProps) {
-  const ticket = {id: 1, description: 'description', assigneeId: 2, completed: false} as Ticket; // mock
-  const status = ticket.completed ? 'Completed' : 'Open';
+  const { id: ticketId } = useParams<{id: string}>();
+  const {data: ticket, isLoading} = useGetTicketByIdQuery(ticketId!);
+
+  if (isLoading) return <div>Loading...</div>
+  if (!isLoading && !ticket) return <div>No ticket found</div>
+
+  const {id, description, assigneeId, completed} = ticket!;
+  
+  const status = completed ? 'Completed' : 'Open';
 
   return (
     <div className={styles['container']}>
       <h2>Ticket Details</h2>
-      <div className={styles['ticket-detail']} key={ticket.id}>
-        <div>ID: {ticket.id}</div>
-        <div>Description: {ticket.description}</div>
-        <div>Assigned To: {ticket.assigneeId}</div>
+      <div className={styles['ticket-detail']} key={id}>
+        <div>ID: {id}</div>
+        <div>Description: {description}</div>
+        <div>Assigned To: {assigneeId}</div>
         <div>Status: {status}</div>
       </div>
     </div>
